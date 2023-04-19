27e7c6f0cecc666b937ef4173388d380ba091586
John Mai <maiqingqiang@foxmail.com>
2023-04-19 17:08:19 +0800

    :heavy_minus_sign:remove rust-crypto,:heavy_plus_sign: use RustCrypto/hashes/md5

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,17 +1,30 @@
-use crypto::digest::Digest;
-use crypto::md5::Md5;
+use md5::Md5;
+use md5::Digest;
 
 pub mod number;
 pub mod token;
 
 pub fn md5(s: &str) -> String {
-    let mut md5 = Md5::new();
-    md5.input_str(s);
-    md5.result_str()
+    let mut hasher = Md5::new();
+    hasher.update(s.as_bytes());
+    format!("{:x}", hasher.finalize())
 }
 
 pub fn md5_string(s: String) -> String {
-    let mut md5 = Md5::new();
-    md5.input_str(s.as_str());
-    md5.result_str()
+    md5(&s)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_md5() {
+        assert_eq!(md5("hello world"), "5eb63bbbe01eeed093cb22bb8f5acdc3");
+    }
+
+    #[test]
+    fn test_md5_string() {
+        assert_eq!(md5_string(String::from("hello world")), "5eb63bbbe01eeed093cb22bb8f5acdc3");
+    }
 }
