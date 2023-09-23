8d45b42566a7be96f94b7e0d5bfc3aa7989d79e6
John Mai <maiqingqiang@foxmail.com>
2023-09-23 16:37:36 +0800

    ⬆️ Upgrade dependencies.

--- src/models/schema.rs
+++ src/models/schema.rs
@@ -3,8 +3,11 @@
 diesel::table! {
     tb_newbee_mall_admin_user (admin_user_id) {
         admin_user_id -> Bigint,
+        #[max_length = 50]
         login_user_name -> Varchar,
+        #[max_length = 50]
         login_password -> Varchar,
+        #[max_length = 50]
         nick_name -> Varchar,
         locked -> Nullable<Tinyint>,
     }
@@ -13,6 +16,7 @@ diesel::table! {
 diesel::table! {
     tb_newbee_mall_admin_user_token (admin_user_id) {
         admin_user_id -> Bigint,
+        #[max_length = 32]
         token -> Varchar,
         update_time -> Datetime,
         expire_time -> Datetime,
@@ -22,7 +26,9 @@ diesel::table! {
 diesel::table! {
     tb_newbee_mall_carousel (carousel_id) {
         carousel_id -> Integer,
+        #[max_length = 100]
         carousel_url -> Varchar,
+        #[max_length = 100]
         redirect_url -> Varchar,
         carousel_rank -> Integer,
         is_deleted -> Tinyint,
@@ -38,6 +44,7 @@ diesel::table! {
         category_id -> Bigint,
         category_level -> Tinyint,
         parent_id -> Bigint,
+        #[max_length = 50]
         category_name -> Varchar,
         category_rank -> Integer,
         is_deleted -> Tinyint,
@@ -51,15 +58,20 @@ diesel::table! {
 diesel::table! {
     tb_newbee_mall_goods_info (goods_id) {
         goods_id -> Unsigned<Bigint>,
+        #[max_length = 200]
         goods_name -> Varchar,
+        #[max_length = 200]
         goods_intro -> Varchar,
         goods_category_id -> Bigint,
+        #[max_length = 200]
         goods_cover_img -> Varchar,
+        #[max_length = 500]
         goods_carousel -> Varchar,
         goods_detail_content -> Text,
         original_price -> Integer,
         selling_price -> Integer,
         stock_num -> Unsigned<Integer>,
+        #[max_length = 20]
         tag -> Varchar,
         goods_sell_status -> Tinyint,
         create_user -> Integer,
@@ -72,9 +84,11 @@ diesel::table! {
 diesel::table! {
     tb_newbee_mall_index_config (config_id) {
         config_id -> Bigint,
+        #[max_length = 50]
         config_name -> Varchar,
         config_type -> Tinyint,
         goods_id -> Bigint,
+        #[max_length = 100]
         redirect_url -> Varchar,
         config_rank -> Integer,
         is_deleted -> Tinyint,
@@ -88,6 +102,7 @@ diesel::table! {
 diesel::table! {
     tb_newbee_mall_order (order_id) {
         order_id -> Bigint,
+        #[max_length = 20]
         order_no -> Varchar,
         user_id -> Bigint,
         total_price -> Integer,
@@ -95,6 +110,7 @@ diesel::table! {
         pay_type -> Tinyint,
         pay_time -> Nullable<Datetime>,
         order_status -> Tinyint,
+        #[max_length = 100]
         extra_info -> Varchar,
         is_deleted -> Tinyint,
         create_time -> Datetime,
@@ -105,11 +121,17 @@ diesel::table! {
 diesel::table! {
     tb_newbee_mall_order_address (order_id) {
         order_id -> Bigint,
+        #[max_length = 30]
         user_name -> Varchar,
+        #[max_length = 11]
         user_phone -> Varchar,
+        #[max_length = 32]
         province_name -> Varchar,
+        #[max_length = 32]
         city_name -> Varchar,
+        #[max_length = 32]
         region_name -> Varchar,
+        #[max_length = 64]
         detail_address -> Varchar,
     }
 }
@@ -119,7 +141,9 @@ diesel::table! {
         order_item_id -> Bigint,
         order_id -> Bigint,
         goods_id -> Bigint,
+        #[max_length = 200]
         goods_name -> Varchar,
+        #[max_length = 200]
         goods_cover_img -> Varchar,
         selling_price -> Integer,
         goods_count -> Integer,
@@ -142,9 +166,13 @@ diesel::table! {
 diesel::table! {
     tb_newbee_mall_user (user_id) {
         user_id -> Bigint,
+        #[max_length = 50]
         nick_name -> Varchar,
+        #[max_length = 11]
         login_name -> Varchar,
+        #[max_length = 32]
         password_md5 -> Varchar,
+        #[max_length = 100]
         introduce_sign -> Varchar,
         is_deleted -> Tinyint,
         locked_flag -> Tinyint,
@@ -156,12 +184,18 @@ diesel::table! {
     tb_newbee_mall_user_address (address_id) {
         address_id -> Bigint,
         user_id -> Bigint,
+        #[max_length = 30]
         user_name -> Varchar,
+        #[max_length = 11]
         user_phone -> Varchar,
         default_flag -> Tinyint,
+        #[max_length = 32]
         province_name -> Varchar,
+        #[max_length = 32]
         city_name -> Varchar,
+        #[max_length = 32]
         region_name -> Varchar,
+        #[max_length = 64]
         detail_address -> Varchar,
         is_deleted -> Tinyint,
         create_time -> Datetime,
@@ -172,6 +206,7 @@ diesel::table! {
 diesel::table! {
     tb_newbee_mall_user_token (user_id) {
         user_id -> Bigint,
+        #[max_length = 32]
         token -> Varchar,
         update_time -> Datetime,
         expire_time -> Datetime,
